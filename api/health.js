6f1cb38cb2ecf996d667d1a09293e9978e104d86
GBSSSCHOOL <dilippawar2207@gmail.com>
2026-06-27 13:14:27 +0530

    School ERP V10 Update

--- api/health.js
+++ api/health.js
@@ -1,4 +1,1 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.status(200).json({ status: true, message: "School ERP V9 AutoPilot API is working" });
-}
+export default function handler(req,res){res.setHeader('Access-Control-Allow-Origin','*');return res.status(200).json({status:true,message:'School ERP V10 AutoPortal API is working'})}
